52f1b4951dbc913650189f1d28fb3cd6651898a4
arena-mode <scarsarena@gmail.com>
2025-03-22 11:10:29 -0600

    Update supabase.js

--- app/utils/supabase.js
+++ app/utils/supabase.js
@@ -1,1 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
 
+const supabaseUrl = 'https://sosrdqwwmyzvnspfmyjd.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNvc3JkcXd3bXl6dm5zcGZteWpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI2NjIwMTAsImV4cCI6MjA1ODIzODAxMH0.3AQ3bXJh-KDw7KMlsLQAm5hkaYJultt3HX4febYhrAQ'
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
