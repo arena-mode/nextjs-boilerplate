6f600b89c4c79997a9fba5fba44e9383f43a294e
arena-mode <scarsarena@gmail.com>
2025-03-22 00:12:50 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Logo from "../components/components/Logo";
-import Navigation from "../components/components/Navigation";
+import Logo from "../../components/components/Logo";
+import Navigation from "../../components/components/Navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
