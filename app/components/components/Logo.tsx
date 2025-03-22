ea9c32a30a8501dd9264b384855944dc12c3d8ec
arena-mode <scarsarena@gmail.com>
2025-03-21 22:35:28 -0600

    Update Logo.tsx
    logo code

--- app/components/components/Logo.tsx
+++ app/components/components/Logo.tsx
@@ -1,1 +1,11 @@
-Screenshot 2025-01-29 172239.png
+import React from 'react';
+
+const Logo = () => {
+  return (
+    <div className="flex items-center">
+      <span className="font-bold text-xl text-white">Crypto Bellwether</span>
+    </div>
+  );
+};
+
+export default Logo;
