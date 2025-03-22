e61d1a01448ce402a6573e630f76162078cd6515
arena-mode <scarsarena@gmail.com>
2025-03-21 19:35:37 -0600

    Create page.tsx

---
+++ app/shorting/page.tsx
@@ -0,0 +1,8 @@
+export default function Shorting() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Shorting</h1>
+      <p>No shorting videos yet.</p>
+    </div>
+  );
+}
