c8e6c036b1e44103a87f7a3beb7dff4c0dd5844f
arena-mode <scarsarena@gmail.com>
2025-03-26 01:54:50 -0600

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // Copy any other configurations from your next.config.ts here
+};
+
+module.exports = nextConfig;
