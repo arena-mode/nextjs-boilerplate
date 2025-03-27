9376554520aec75873ecd77c342644f118898b4f
Crypto Bellwether <your-email@example.com>
2025-03-26 23:32:42 -0600

    feat: add TestComponent and update ESLint config

---
+++ src/components/TestComponent.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const TestComponent = ({ name }: { name: string }) => {
+  console.log('test');
+  return (
+    <div>
+      <h1>Hello, {name}\!</h1>
+    </div>
+  );
+};
